20e8af2ad019aac29148e735756158409356cbcb
Crisu83 <ChristofferNiska@gmail.com>
2012-12-29 03:25:32 +0200

    Add missing comments

--- AuthModule.php
+++ AuthModule.php
@@ -1,71 +1,110 @@
 <?php
+/**
+ * AuthModule class file.
+ * @author Christoffer Niska <ChristofferNiska@gmail.com>
+ * @copyright Copyright &copy; Christoffer Niska 2012-
+ * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @package auth
+ * @version 1.0.0
+ */
 
+/**
+ * Web module for managing Yii's built-in authorization manager (CAuthManager).
+ */
 class AuthModule extends CWebModule
 {
-    public $defaultController = 'assignment';
-    public $appLayout = 'application.views.layouts.main';
-    public $strictMode = true;
-    public $forceCopyAssets = false;
-    public $users = array('admin');
+	/**
+	 * @var string string the id of the default controller for this module.
+	 */
+	public $defaultController = 'assignment';
+	/**
+	 * @var string the application layout.
+	 */
+	public $appLayout = 'application.views.layouts.main';
+	/**
+	 * @var boolean whether to enable the RBAC strict mode.
+	 * When enabled items cannot be assigned children of the same type.
+	 */
+	public $strictMode = true;
+	/**
+	 * @var boolean whether to force copying of assets.
+	 */
+	public $forceCopyAssets = false;
+	/**
+	 * @var array list of names for users that has access to this module.
+	 */
+	public $users = array('admin');
+	/**
+	 * @var string name of the user model class.
+	 */
+	public $userClass = 'User';
+	/**
+	 * @var string name of the user id column.
+	 */
+	public $userIdColumn = 'id';
+	/**
+	 * @var string name of the user name column.
+	 */
+	public $userNameColumn = 'name';
 
-    public $userClass = 'User';
-    public $userIdColumn = 'id';
-    public $userNameColumn = 'name';
+	private $_assetsUrl;
 
-    private $_assetsUrl;
+	/**
+	 * Initializes the module.
+	 */
+	public function init()
+	{
+		$this->setImport(array(
+			'auth.components.*',
+			'auth.models.*',
+			'auth.widgets.*',
+		));
 
-    public function init()
-    {
-        // import the module-level models and components
-        $this->setImport(
-            array(
-                'auth.components.*',
-                'auth.models.*',
-                'auth.widgets.*',
-            )
-        );
+		$this->registerCss();
+	}
 
-        $this->registerCss();
-    }
+	/**
+	 * Registers the module CSS.
+	 */
+	public function registerCss()
+	{
+		Yii::app()->clientScript->registerCssFile($this->getAssetsUrl() . '/css/auth.css');
+	}
 
-    public function registerCss()
-    {
-        Yii::app()->clientScript->registerCssFile($this->getAssetsUrl() . '/css/auth.css');
-    }
+	/**
+	 * The pre-filter for controller actions.
+	 * @param CController $controller the controller.
+	 * @param CAction $action the action.
+	 * @return boolean whether the action should be executed.
+	 * @throws CHttpException if user is denied access.
+	 */
+	public function beforeControllerAction($controller, $action)
+	{
+		if (parent::beforeControllerAction($controller, $action))
+		{
+			if (!in_array(Yii::app()->user->getName(), $this->users))
+				throw new CHttpException(401, Yii::t('AuthModule.main', 'Access denied.'));
 
-    /**
-     * @param CController $controller
-     * @param CAction $action
-     * @return boolean
-     * @throws CHttpException
-     */
-    public function beforeControllerAction($controller, $action)
-    {
-        if (parent::beforeControllerAction($controller, $action))
-        {
-            if (!in_array(Yii::app()->user->getName(), $this->users))
-                throw new CHttpException(401, 'Access denied.');
+			return true;
+		}
+		else
+			return false;
+	}
 
-            return true;
-        }
-        else
-            return false;
-    }
+	/**
+	 * Returns the URL to the published assets folder.
+	 * @return string the URL.
+	 */
+	protected function getAssetsUrl()
+	{
+		if (isset($this->_assetsUrl))
+			return $this->_assetsUrl;
+		else
+		{
+			$assetsPath = Yii::getPathOfAlias('auth.assets');
+			$assetsUrl = Yii::app()->assetManager->publish($assetsPath, false, -1, $this->forceCopyAssets);
 
-    /**
-     * Returns the URL to the published assets folder.
-     * @return string the URL
-     */
-    protected function getAssetsUrl()
-    {
-        if (isset($this->_assetsUrl))
-            return $this->_assetsUrl;
-        else
-        {
-            $assetsPath = Yii::getPathOfAlias('auth.assets');
-            $assetsUrl = Yii::app()->assetManager->publish($assetsPath, false, -1, $this->forceCopyAssets);
-
-            return $this->_assetsUrl = $assetsUrl;
-        }
-    }
+			return $this->_assetsUrl = $assetsUrl;
+		}
+	}
 }
