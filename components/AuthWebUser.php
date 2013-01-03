255818b5108a7bd4b288cd5a1fe92f44aa35944a
Crisu83 <ChristofferNiska@gmail.com>
2013-01-03 23:59:49 +0200

    Add web user component

---
+++ components/AuthWebUser.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * AuthWebUser class file.
+ * @author Christoffer Niska <ChristofferNiska@gmail.com>
+ * @copyright Copyright &copy; Christoffer Niska 2013-
+ * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @package auth.components
+ */
+
+/**
+ * Web user that allows for passing access checks when enlisted as an administrator.
+ */
+class AuthWebUser extends CWebUser
+{
+	/**
+	 * @var string[] a list of names for the users that should be treated as administrators.
+	 */
+	public $admins = array('admin');
+
+	/**
+	 * Initializes the component.
+	 */
+	public function init()
+	{
+		parent::init();
+		$this->setIsAdmin($this->checkAdmin());
+	}
+
+	/**
+	 * Returns whether the logged in user is an administrator.
+	 * @return boolean the result.
+	 */
+	public function getIsAdmin()
+	{
+		return $this->getState('__isAdmin');
+	}
+
+	/**
+	 * Sets the logged in user as an administrator.
+	 * @param boolean $value whether the user is an administrator.
+	 */
+	public function setIsAdmin($value)
+	{
+		$this->setState('__isAdmin', $value);
+	}
+
+	/**
+	 * Checks if the logged in user is an administrator.
+	 * @return boolean the result.
+	 */
+	protected function checkAdmin()
+	{
+		return in_array($this->name, $this->admins);
+	}
+
+	/**
+	 * Performs access check for this user.
+	 * @param string $operation the name of the operation that need access check.
+	 * @param array $params name-value pairs that would be passed to business rules associated
+	 * with the tasks and roles assigned to the user.
+	 * @param boolean $allowCaching whether to allow caching the result of access check.
+	 * @return boolean whether the operations can be performed by this user.
+	 */
+	public function checkAccess($operation, $params = array(), $allowCaching = true)
+	{
+		if ($this->checkAdmin())
+			return true;
+
+		return parent::checkAccess($operation, $params, $allowCaching);
+	}
+}
