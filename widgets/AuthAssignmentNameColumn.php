94cfab1aabf1bede869f47b3a513b8ae9a47d5a5
cstdenis <cstdenis@ctgameinfo.com>
2013-06-19 13:31:57 -0600

    Update AuthAssignmentNameColumn.php
    Override renderFilterCellContent() to have a text field.

--- widgets/AuthAssignmentNameColumn.php
+++ widgets/AuthAssignmentNameColumn.php
@@ -32,4 +32,12 @@ class AuthAssignmentNameColumn extends AuthAssignmentColumn
 	{
 		echo CHtml::link(CHtml::value($data, $this->nameColumn), array('view', 'id'=>$data->{$this->idColumn}));
 	}
+
+        /**
+         * Renders the filter cell content.
+         */
+        protected function renderFilterCellContent()
+        {
+                echo CHtml::activeTextField($this->grid->filter, $this->getNameColumn(), array('id'=>false));
+        }
 }
