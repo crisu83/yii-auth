57caa80e9c4321559db79ce7d09ae389dcc8ab25
Ilko Kacharov <ilko.kacharov@opencode.com>
2013-06-20 11:07:31 +0300

    Tabs converted to two spaces.

--- views/authItem/update.php
+++ views/authItem/update.php
@@ -5,34 +5,34 @@
 /* @var $form TbActiveForm */
 
 $this->breadcrumbs = array(
-	$this->capitalize($this->getTypeText(true)) => array('index'),
-	$item->description => array('view', 'name' => $item->name),
-	Yii::t('AuthModule.main', 'Edit'),
+  $this->capitalize($this->getTypeText(true)) => array('index'),
+  $item->description => array('view', 'name' => $item->name),
+  Yii::t('AuthModule.main', 'Edit'),
 );
 ?>
 
 <h1>
-	<?php echo CHtml::encode($item->description); ?>
-	<small><?php echo $this->getTypeText(); ?></small>
+  <?php echo CHtml::encode($item->description); ?>
+  <small><?php echo $this->getTypeText(); ?></small>
 </h1>
 
 <?php $form = $this->beginWidget('bootstrap.widgets.TbActiveForm'); ?>
 
 <?php echo $form->hiddenField($model, 'type'); ?>
 <?php echo $form->textFieldControlGroup($model, 'name', array(
-	'disabled'=>true,
-	'title'=>Yii::t('AuthModule.main', 'System name cannot be changed after creation.'),
+  'disabled'=>true,
+  'title'=>Yii::t('AuthModule.main', 'System name cannot be changed after creation.'),
 )); ?>
 <?php echo $form->textFieldControlGroup($model, 'description'); ?>
 
 <div class="form-actions">
-	<?php echo TbHtml::submitButton(Yii::t('AuthModule.main', 'Save'),array(
-		'color'=>TbHtml::BUTTON_COLOR_PRIMARY,
-	)); ?>`
-	<?php echo TbHtml::linkButton(Yii::t('AuthModule.main', 'Cancel'),array(
-		'color'=>TbHtml::BUTTON_COLOR_LINK,
-		'url' => array('view', 'name' => $item->name),
-	)); ?>
+  <?php echo TbHtml::submitButton(Yii::t('AuthModule.main', 'Save'),array(
+    'color'=>TbHtml::BUTTON_COLOR_PRIMARY,
+  )); ?>`
+  <?php echo TbHtml::linkButton(Yii::t('AuthModule.main', 'Cancel'),array(
+    'color'=>TbHtml::BUTTON_COLOR_LINK,
+    'url' => array('view', 'name' => $item->name),
+  )); ?>
 </div>
 
 <?php $this->endWidget(); ?>
